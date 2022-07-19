30915bce6b9bc5d7864975f955482f9f0c21e396
NehlTech <obololastkiller@gmail.com>
2022-07-19 05:29:54 +0000

    Safe Function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -20,6 +20,8 @@ def safe_function(fct, *args):
         result = fct(*args)
         return (result)
         
-    except:
+    except Exception as error:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
+
+
