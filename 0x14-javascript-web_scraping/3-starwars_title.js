f052bf5cd97660857688f734797b1ff46868ddf1
bright <obololastkiller@gmail.com>
2022-11-17 05:16:55 +0000

    Print Title of Movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require("request");
-let url = "http://swapi.co/api/films/" + process.argv[2];
+let url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
 request(url, function (error, response, body) {
   if (error) throw error;
   console.log(JSON.parse(body).title);
